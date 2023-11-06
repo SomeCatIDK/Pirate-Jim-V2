30c40703c6e6b2cfa03721f2fb2b300b6ea71834
SomeCatIDK <graybad1@icloud.com>
2023-11-06 17:56:35 -0500

    file-scoped namespaces in new files

--- src/Model/UORoles.cs
+++ src/Model/UORoles.cs
@@ -1,63 +1,55 @@
-﻿using Discord;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SomeCatIDK.PirateJim.Model;
 
-namespace SomeCatIDK.PirateJim.src.Model
+public static class UORoles
 {
-    public static class UORoles
+    public const ulong SDG = 324232061093412867;
+    public const ulong ModerationTeam = 326108032067174411;
+    public const ulong Adminstrator = 324230861245513729;
+    public const ulong Moderator = 324230866702434305;
+    public const ulong JuniorModerator = 900374139452395530;
+    public const ulong Supporter = 553316077636157451;
+    public const ulong CommunityStaff = 521032653089013800;
+    public const ulong Bots = 553308177756585995;
+    public const ulong ModdingSupport = 553315866146766850;
+    public const ulong GameSupport = 340042787896426496;
+    public const ulong BotBoio = 614492148162625677;
+    public const ulong Muted = 700375522412199996;
+    public const ulong ThreadsBanned = 1083102466175926292;
+    public const ulong ReactionMuted = 685230253819429048;
+    public const ulong LivestreamMuted = 732627475200213012;
+    public const ulong ModdingResources = 872654242165444709;
+    public const ulong ArtContestWinner = 710738372414537728;
+    public const ulong ContentCreator = 335740807753629699;
+    public const ulong Contributor = 527922530401517586;
+    public const ulong YouTube = 335740119996694530;
+    public const ulong Twitch = 335740127462555661;
+    public const ulong Workshop = 335762652225601536;
+    public const ulong DiscordContestWinner = 816862441832448063;
+    public const ulong SpecialThanks = 559385313890271244;
+    public const ulong CrimsonBeret = 568778449779425293;
+    public const ulong WhiteHatter = 568776874071687169;
+    public const ulong Debugger = 568776870858850323;
+    public const ulong NitroBooster = 585535333584732191;
+    public const ulong Experienced = 568776711467040779;
+    public const ulong Gold = 324231297096613888;
+    public const ulong EarlyAccess = 333173979801976834;
+    public const ulong Survivor = 554431731344736258;
+
+
+    public static readonly ulong[] GrantableRoles =
     {
-        public const ulong SDG = 324232061093412867;
-        public const ulong ModerationTeam = 326108032067174411;
-        public const ulong Adminstrator = 324230861245513729;
-        public const ulong Moderator = 324230866702434305;
-        public const ulong JuniorModerator = 900374139452395530;
-        public const ulong Supporter = 553316077636157451;
-        public const ulong CommunityStaff = 521032653089013800;
-        public const ulong Bots = 553308177756585995;
-        public const ulong ModdingSupport = 553315866146766850;
-        public const ulong GameSupport = 340042787896426496;
-        public const ulong BotBoio = 614492148162625677;
-        public const ulong Muted = 700375522412199996;
-        public const ulong ThreadsBanned = 1083102466175926292;
-        public const ulong ReactionMuted = 685230253819429048;
-        public const ulong LivestreamMuted = 732627475200213012;
-        public const ulong ModdingResources = 872654242165444709;
-        public const ulong ArtContestWinner = 710738372414537728;
-        public const ulong ContentCreator = 335740807753629699;
-        public const ulong Contributor = 527922530401517586;
-        public const ulong YouTube = 335740119996694530;
-        public const ulong Twitch = 335740127462555661;
-        public const ulong Workshop = 335762652225601536;
-        public const ulong DiscordContestWinner = 816862441832448063;
-        public const ulong SpecialThanks = 559385313890271244;
-        public const ulong CrimsonBeret = 568778449779425293;
-        public const ulong WhiteHatter = 568776874071687169;
-        public const ulong Debugger = 568776870858850323;
-        public const ulong NitroBooster = 585535333584732191;
-        public const ulong Experienced = 568776711467040779;
-        public const ulong Gold = 324231297096613888;
-        public const ulong EarlyAccess = 333173979801976834;
-        public const ulong Survivor = 554431731344736258;
-
-
-        public static readonly ulong[] GrantableRoles =
-        {
-            ArtContestWinner,
-            ContentCreator,
-            Contributor, 
-            YouTube, 
-            Twitch, 
-            Workshop, 
-            DiscordContestWinner, 
-            SpecialThanks, 
-            CrimsonBeret, 
-            Gold, 
-            EarlyAccess, 
-            WhiteHatter, 
-            Debugger
-        };
-    }
+        ArtContestWinner,
+        ContentCreator,
+        Contributor, 
+        YouTube, 
+        Twitch, 
+        Workshop, 
+        DiscordContestWinner, 
+        SpecialThanks, 
+        CrimsonBeret, 
+        Gold, 
+        EarlyAccess, 
+        WhiteHatter, 
+        Debugger
+    };
 }
--- src/Modules/RoleCommandModule.cs
+++ src/Modules/RoleCommandModule.cs
@@ -1,43 +1,36 @@
-﻿using Discord;
-using Discord.Interactions;
-using Discord.Rest;
-using Discord.WebSocket;
-using SomeCatIDK.PirateJim.Model;
-using SomeCatIDK.PirateJim.src.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using SomeCatIDK.PirateJim.Model;
 
-namespace SomeCatIDK.PirateJim.src.Modules
+namespace SomeCatIDK.PirateJim.Modules;
+
+[Group("role", "Grants or revokes the specified role from the user.")]
+public class RoleCommandModule : InteractionModuleBase
 {
-    [Group("role", "Grants or revokes the specified role from the user.")]
-    public class RoleCommandModule : InteractionModuleBase
+    [RequireRole(UORoles.ModerationTeam)]
+    [SlashCommand("grant", "Grants the specified role to the user.")]
+    public async Task GrantRole(IGuildUser user, IRole role)
     {
-        [RequireRole(UORoles.ModerationTeam)]
-        [SlashCommand("grant", "Grants the specified role to the user.")]
-        public async Task GrantRole(IGuildUser user, IRole role)
-        {
-            if (!UORoles.GrantableRoles.Contains(role.Id)){
-                await RespondAsync("The specified role cannot be granted.");
-                return;
-            }
-            await user.AddRoleAsync(role);
-            await RespondAsync($"Role granted. ✅");
+        if (!UORoles.GrantableRoles.Contains(role.Id)){
+            await RespondAsync("The specified role cannot be granted.");
+            return;
         }
+        await user.AddRoleAsync(role);
+        await RespondAsync($"Role granted. ✅");
+    }
 
-        [RequireRole(UORoles.ModerationTeam)]
-        [SlashCommand("revoke", "Revokes the specified role from the user.")]
-        public async Task RevokeRole(IGuildUser user, IRole role)
+    [RequireRole(UORoles.ModerationTeam)]
+    [SlashCommand("revoke", "Revokes the specified role from the user.")]
+    public async Task RevokeRole(IGuildUser user, IRole role)
+    {
+        if (!UORoles.GrantableRoles.Contains(role.Id))
         {
-            if (!UORoles.GrantableRoles.Contains(role.Id))
-            {
-                await RespondAsync("The specified role cannot be revoked.");
-                return;
-            }
-            await user.RemoveRoleAsync(role);
-            await RespondAsync($"Role revoked. ✅");
+            await RespondAsync("The specified role cannot be revoked.");
+            return;
         }
+        await user.RemoveRoleAsync(role);
+        await RespondAsync($"Role revoked. ✅");
     }
 }
