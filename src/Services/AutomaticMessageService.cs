20afc09a1573cd9e2fc58d811eeef6e0308ce214
Gamingtoday093 <Gamingtoday093@gmail.com>
2025-01-19 16:49:22 +0100

    Save Automatic Last Messages to the DB

--- src/Services/AutomaticMessageService.cs
+++ src/Services/AutomaticMessageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using SomeCatIDK.PirateJim.Model;
@@ -9,18 +10,12 @@ namespace SomeCatIDK.PirateJim.Services;
 
 public class AutomaticMessageService : IService
 {
-    public const string AdvertisingMessage = "Welcome to #advertising.\n\nPlease note that you may only post advertisements **every 23 hours**. Advertisements should not promote servers that:\n- Are not relevant to the game *Unturned*.\n- Promote the usage of game cheats.\n- Actively go against [Server Hosting Rules](<https://docs.smartlydressedgames.com/en/stable/servers/server-hosting-rules.html>).\n- Use workarounds to avoid copyright claims, host bans, or other consequences regarding illegal or disallowed practices (i.e. having players manually install workshop mods).";
+    public readonly string AdvertisingMessage = $"Welcome to <#{UOChannels.Advertising}>.\n\nPlease note that you may only post advertisements **every 23 hours**. Advertisements should not promote servers that:\n- Are not relevant to the game *Unturned*.\n- Promote the usage of game cheats.\n- Actively go against [Server Hosting Rules](<https://docs.smartlydressedgames.com/en/stable/servers/server-hosting-rules.html>).\n- Use workarounds to avoid copyright claims, host bans, or other consequences regarding illegal or disallowed practices (i.e. having players manually install workshop mods).";
 
-    public const string TradingMessage = "Welcome to #trading.\n\n**__Warning!__**\n You should **ONLY** trade through Steam. **DO NOT** use PayPal, Venmo, or any other service to conduct a trade. **DO NOT** trade for real money.\n\nDo not advertise:\n- Real-money trades (RMT)\n- Scams, cheats, or automation tools\n- Accounts\n\n__We cannot help you if you've lost your items, and didn't receive what you were supposed to!__\nPlease read this [Steam page](<https://help.steampowered.com/en/faqs/view/18A5-167F-C27B-64A0>).";
-
-    public const string LookingForGroupMessage = " Welcome to #looking-for-group.\n\nYou may not:\n- Advertise clans or other discord servers.\n - Promote the usage of game cheats.\n - Share servers in which you are directly affiliated with the staff or owner.";
-
-    public ulong? LastAdvertisingMessage;
-
-    public ulong? LastTradingMessage;
-
-    public ulong? LastLookingForGroupMessage;
-
+    public readonly string TradingMessage = $"Welcome to <#{UOChannels.Trading}>.\n\n**__Warning!__**\n You should **ONLY** trade through Steam. **DO NOT** use PayPal, Venmo, or any other service to conduct a trade. **DO NOT** trade for real money.\n\nDo not advertise:\n- Real-money trades (RMT)\n- Scams, cheats, or automation tools\n- Accounts\n\n__We cannot help you if you've lost your items, and didn't receive what you were supposed to!__\nPlease read this [Steam page](<https://help.steampowered.com/en/faqs/view/18A5-167F-C27B-64A0>).";
+#if RELEASE
+    public readonly string LookingForGroupMessage = $" Welcome to <#{UOChannels.LookingForGroup}>.\n\nYou may not:\n- Advertise clans or other discord servers.\n - Promote the usage of game cheats.\n - Share servers in which you are directly affiliated with the staff or owner.";
+#endif
     public AutomaticMessageService(PirateJim bot)
     {
         bot.DiscordClient.MessageReceived += OnMessage;
@@ -68,29 +63,32 @@ public class AutomaticMessageService : IService
         switch (message.Channel.Id)
         {
             case UOChannels.Advertising:
-                var firstMsg = await message.Channel.SendMessageAsync(AdvertisingMessage);
+                var advertisingMessage = await message.Channel.SendMessageAsync(AdvertisingMessage);
 
-                if (LastAdvertisingMessage != null)
-                    await message.Channel.DeleteMessageAsync(LastAdvertisingMessage.Value);
+                ulong? oldAdvertisingMessage = await GetAndUpdateOldLastMessage(message.Channel.Id, advertisingMessage.Id);
 
-                LastAdvertisingMessage = firstMsg.Id;
+                if (oldAdvertisingMessage != null)
+                    await message.Channel.DeleteMessageAsync(oldAdvertisingMessage.Value);
+
                 break;
             case UOChannels.Trading:
-                var secondMsg = await message.Channel.SendMessageAsync(TradingMessage);
+                var tradingMessage = await message.Channel.SendMessageAsync(TradingMessage);
 
-                if (LastTradingMessage != null)
-                    await message.Channel.DeleteMessageAsync(LastTradingMessage.Value);
+                ulong? oldTradingMessage = await GetAndUpdateOldLastMessage(message.Channel.Id, tradingMessage.Id);
 
-                LastTradingMessage = secondMsg.Id;
+                if (oldTradingMessage != null)
+                    await message.Channel.DeleteMessageAsync(oldTradingMessage.Value);
+
                 break;
 #if RELEASE
             case UOChannels.LookingForGroup:
-                var thirdMsg = await message.Channel.SendMessageAsync(LookingForGroupMessage);
+                var lookingForGroupMessage = await message.Channel.SendMessageAsync(LookingForGroupMessage);
 
-                if (LastLookingForGroupMessage != null)
-                    await message.Channel.DeleteMessageAsync(LastLookingForGroupMessage.Value);
+                ulong? oldLookingForGroupMessage = await GetAndUpdateOldLastMessage(message.Channel.Id, lookingForGroupMessage.Id);
 
-                LastLookingForGroupMessage = thirdMsg.Id;
+                if (oldLookingForGroupMessage != null)
+                    await message.Channel.DeleteMessageAsync(oldLookingForGroupMessage.Value);
+
                 break;
 #endif
             case UOChannels.Modding:
@@ -104,6 +102,37 @@ public class AutomaticMessageService : IService
         }
 
         await Task.CompletedTask;
+    }
+
+    private async Task<ulong?> GetAndUpdateOldLastMessage(ulong channelId, ulong newLastMessage)
+    {
+        await using var db = new PirateJimDbContext();
+
+        var lastMessageChannel = db.LastMessageChannels
+            .FirstOrDefault(c => c.ChannelId == channelId);
+
+        ulong? oldLastMessage = lastMessageChannel?.MessageId;
+
+        if (lastMessageChannel == null)
+        {
+            lastMessageChannel = new LastMessageChannel
+            {
+                ChannelId = channelId,
+                MessageId = newLastMessage
+            };
+
+            await db.LastMessageChannels.AddAsync(lastMessageChannel);
+        }
+        else
+        {
+            lastMessageChannel.MessageId = newLastMessage;
+
+            db.LastMessageChannels.Update(lastMessageChannel);
+        }
+
+        await db.SaveChangesAsync();
+
+        return oldLastMessage;
     }
 }
 
