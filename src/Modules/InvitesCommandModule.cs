a06b0fc8971ad2c00de9217c4b85f71cfb1dcfbf
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2025-02-18 18:13:37 -0500

    Added  clearnonrelevant invite command.

---
+++ src/Modules/InvitesCommandModule.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using SomeCatIDK.PirateJim.Model;
+
+
+namespace SomeCatIDK.PirateJim.src.Modules
+{
+    [Group("invites", "Commands that manage invites.")]
+    public class InvitesCommandModule : InteractionModuleBase
+    {
+        [RequireRole(UORoles.ModerationTeam)]
+        [SlashCommand("clearnonrelevant", "Clears all non-relevant invites.")]
+        public async Task ClearNonRelevant()
+        {
+            var invites = await Context.Guild.GetInvitesAsync();
+
+            int num = 0;
+
+            foreach (var invite in invites)
+            {
+                if (invite.Inviter.Id == 192338846971723776 || invite.Inviter.Id == 178374897897177088)
+                    continue;
+
+                if (invite.Uses >= 10)
+                    continue;
+
+                await invite.DeleteAsync();
+                num++;
+            }
+
+            await RespondAsync($"Deleted {num} invites.");
+        }
+    }
+}
