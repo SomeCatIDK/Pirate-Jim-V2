2e7170f9c0f9c4f06eded898fb335ba7a9bd123d
SomeCatIDK <graybad1@icloud.com>
2026-02-16 18:15:39 -0500

    Update dotnet and dependencies

--- src/HTTP/Extensions/ResponseBuilder.cs
+++ src/HTTP/Extensions/ResponseBuilder.cs
@@ -2,20 +2,21 @@
 using System.Text.Json;
 using System.Threading.Tasks;
 using GenHTTP.Api.Protocol;
-using GenHTTP.Modules.Conversion.Providers.Json;
+using GenHTTP.Modules.Conversion.Serializers.Json;
 using SomeCatIDK.PirateJim.HTTP.Model;
 
 namespace SomeCatIDK.PirateJim.HTTP.Extensions;
 
 public static class ResponseBuilder
 {
-    public static ValueTask<IResponse?> BuildJsonResponse(this IResponseBuilder builder, ResponseStatus status, object content)
+    public static ValueTask<IResponse?> BuildJsonResponse(this IRequest request, ResponseStatus status, object content)
     {
         // Creates a C# object representing the content of the message.
-        var response = new ResponseRecord($"{(int)status} - {status.ToString()}", DateTime.UtcNow, builder.Request.Host + builder.Request.Target.Path, content);
+        var response = new ResponseRecord($"{(int)status} - {status.ToString()}", DateTime.UtcNow, request.Host + request.Target.Path, content);
         
         // Build a pretty-printed JSON response with application/json header
-        return new ValueTask<IResponse?>(builder
+        return new ValueTask<IResponse?>(request
+            .Respond()
             .Status(status)
             .Content(new JsonContent(response, new JsonSerializerOptions { WriteIndented = true }))
             .Type(FlexibleContentType.Get(ContentType.ApplicationJson))
--- src/HTTP/JsonErrorMapper.cs
+++ src/HTTP/JsonErrorMapper.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using GenHTTP.Api.Content;
 using GenHTTP.Api.Protocol;
+using GenHTTP.Modules.ErrorHandling;
 using SomeCatIDK.PirateJim.HTTP.Extensions;
 using SomeCatIDK.PirateJim.HTTP.Model;
 
@@ -14,14 +15,14 @@ public class JsonErrorMapper : IErrorMapper<Exception>
     {
         Console.WriteLine(error.Message + error.StackTrace);
         
-        return await request.Respond()
+        return await request
             .BuildJsonResponse(ResponseStatus.InternalServerError, new MessageRecord("An internal error occured, please report this to an administrator: " + error.Message));
     }
 
     // 404
     public async ValueTask<IResponse?> GetNotFound(IRequest request, IHandler handler)
     {
-        return await request.Respond()
+        return await request
             .BuildJsonResponse(ResponseStatus.NotFound, new MessageRecord("The requested endpoint does not exist."));
     }
 }
